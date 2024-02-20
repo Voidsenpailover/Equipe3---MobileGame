f1ce7cf51e754f6b32cb46b1da56f6baa8a2d820
FrancisBullock <144137562+FrancisBullock@users.noreply.github.com>
2024-02-20 15:52:12 +0100

    Ajout de script

---
+++ Assets/ScriptFrancis/Tourelle Stat.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TourelleStat : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] string name;
+    [SerializeField] int dégats;
+    [SerializeField] int délai;
+    [SerializeField] int cout;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
