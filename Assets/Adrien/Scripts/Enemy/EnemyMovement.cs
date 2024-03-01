fa1e21700a96466764c8c2764859759603d4f9d5
Lombard Jérémy <jeremylombard.mail@gmail.com>
2024-03-01 16:25:41 +0100

    shader

--- Assets/Adrien/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Adrien/Scripts/Enemy/EnemyMovement.cs
@@ -24,7 +24,7 @@ using System;
 
     private void Update()
     {
-      if (!reachedEnd && Vector2.Distance(target.position, transform.position) <= 0.1f)
+      if (!reachedEnd && Vector2.Distance(target.position, transform.position) <= 0.0001f)
       {
         Point++;
         if (Point >= LevelManager.instance.Chemin.Length)
