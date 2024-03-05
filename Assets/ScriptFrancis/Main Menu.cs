1b97195d1265ba8e582c1cf2f013314f80882ffb
FrancisBullock <144137562+FrancisBullock@users.noreply.github.com>
2024-03-05 15:28:20 +0100

    Création d'un menu

---
+++ Assets/ScriptFrancis/Main Menu.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject creditScreen;
+    public GameObject firstchooseScreen;
+    public LevelManager levelManager;
+    public void StartGame()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);/*En cas de Scene Manager*/
+        //SceneManager.LoadSceneAsync(1);/*Copier coller d'un morceau de code dans le script Level Manager*/
+
+    }
+    public void ShowCredit()
+    {
+        creditScreen.SetActive(true);
+        firstchooseScreen.SetActive(false);
+    }
+    public void HideCredit()
+    {
+        creditScreen.SetActive(false);
+        firstchooseScreen.SetActive(true);
+    }
+        
+
+    public void Quitter()
+    {
+        Application.Quit();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
