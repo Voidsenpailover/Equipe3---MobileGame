8a91e5808a0ffde3796964717f0c7bdd4b7024cc
adrien-deroffignac <adrien.de_roffignac@edu.devinci.fr>
2024-03-10 16:07:53 +0100

    Lexique Tours

---
+++ Assets/Adrien/Scripts/LexiqueTurret.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LexiqueTurret : MonoBehaviour
+{
+    
+    [SerializeField] private TurretsData TourPrincipale;
+    [SerializeField] private TurretsData TourNiveauApres;
+    [SerializeField] private Sprite SpriteTour;
+    [SerializeField] private TextMeshProUGUI Name;
+    [SerializeField] private TextMeshProUGUI Description;
+    [SerializeField] private TextMeshProUGUI Cost;
+    [SerializeField] private TextMeshProUGUI SellPrice;
+    [SerializeField] private TextMeshProUGUI Atk;
+    [SerializeField] private TextMeshProUGUI AtkSpeed;
+    [SerializeField] private TextMeshProUGUI Range;
+    [SerializeField] private Sprite SpriteTourMax;
+    
+    
+    void Start()
+    {
+        SpriteTour = TourPrincipale.Sprite;
+        Name.text = TourPrincipale.Name;
+        Description.text = TourPrincipale.Description;
+        Cost.text = TourPrincipale.Cost.ToString();
+        SellPrice.text = (TourPrincipale.Cost / 2).ToString();
+        Atk.text = TourPrincipale.Damage + "dgts";
+        AtkSpeed.text = TourPrincipale.DelayBetweenAtk.ToString("0.0") + "Atk/s";
+        Range.text = TourPrincipale.RadAtk.ToString("0.0");
+        SpriteTourMax = TourNiveauApres.Sprite;
+    }
+
+    
+}
