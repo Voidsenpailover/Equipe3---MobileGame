05523ee601e67e9ed2cbe0fbd6630fc590d80154
Yann Gaudillat <ishadows828@gmail.com>
2024-03-13 00:34:12 +0100

    SO Ennemies 1 -> 25

--- Assets/Jerem/Script/UI/UiManager.cs
+++ Assets/Jerem/Script/UI/UiManager.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UiManager : MonoBehaviour
 {
     [SerializeField] RectTransform _turretMenu;
     [SerializeField] GameObject _menuRotationPoint;
-
+    [SerializeField] TextMeshProUGUI _waveText;
+    [SerializeField] TextMeshProUGUI _maxWaveText;
+    [SerializeField] TextMeshProUGUI _moneyText;
+    [SerializeField] TextMeshProUGUI _healthText;
     void Start()
     {
-        GridBuildingSystem.OnTurretMenuActive += SetTurretMenu;
-        GridBuildingSystem.OnTurretMenuDeactivated += UnsetTurretMenu;
+        _waveText.text = EnemySpawner._instance._currentRoundIndex.ToString();
+        _maxWaveText.text = EnemySpawner._instance._rounds.Count.ToString();
+        _moneyText.text = LevelManager.instance.money.ToString();
+        _healthText.text = LevelManager.instance.HP.ToString();
+    }
+    private void UnsetTurretMenu()
+    {
+        _turretMenu.position = Vector3.zero;
+        _menuRotationPoint.SetActive(false);
     }
 
     private void SetTurretMenu(Vector3 pos)
