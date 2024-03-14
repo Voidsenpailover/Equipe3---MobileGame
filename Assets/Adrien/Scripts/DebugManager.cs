4f6eb34a3c50838a08146f50c64fd1327d05f668
adrien-deroffignac <adrien.de_roffignac@edu.devinci.fr>
2024-03-14 17:42:00 +0100

    fix bug

--- Assets/Adrien/Scripts/DebugManager.cs
+++ Assets/Adrien/Scripts/DebugManager.cs
@@ -2,23 +2,14 @@ using UnityEngine;
 
     public class DebugManager : MonoBehaviour
     { 
-        [Header("Touches Debug")]
-        [SerializeField] private KeyCode StartGame;
-
+        
         [SerializeField] private GameObject _mainMenu;
         [SerializeField]private AudioSource _audioSource;
         [SerializeField] private AudioClip _musiqueDeFond;
         [SerializeField] private AudioClip _musiqueDeFond2;
     
-    #if UNITY_EDITOR    
         private void Update()
         {
-            if (Input.GetKeyDown(StartGame))
-            {
-                LevelManager.instance.StartGame();
-            }
-            
-    #endif
             if(LevelManager.instance.CurrentState == LevelManager.GameState.MainMenu)
             {
                 _audioSource.clip = _musiqueDeFond;
