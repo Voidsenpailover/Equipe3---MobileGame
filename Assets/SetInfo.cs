9758d60aa2070161f5840e76dea772db0346454e
Lombard Jérémy <jeremylombard.mail@gmail.com>
2024-03-14 15:34:32 +0100

    Prefab For Selection

--- Assets/SetInfo.cs
+++ Assets/SetInfo.cs
@@ -16,7 +16,15 @@ public class SetInfo : MonoBehaviour
         _TurretName.text = turret.Name;
         _TurretAtk.text = turret.Damage.ToString("0.0");
         _TurretAtkSpeed.text = turret.DelayBetweenAtk.ToString("0.0");
-        var cout = turret.Cost * 0.6f;
+        var cout = turret.Cost * 0.5f;
+        _TurretCost.text = cout.ToString("0.0");
+    }
+    public void SetTurretFusion(TurretsData turret)
+    {
+        _TurretName.text = turret.Name;
+        _TurretAtk.text = turret.Damage.ToString("0.0");
+        _TurretAtkSpeed.text = turret.DelayBetweenAtk.ToString("0.0");
+        var cout = turret.Cost;
         _TurretCost.text = cout.ToString("0.0");
     }
 }
